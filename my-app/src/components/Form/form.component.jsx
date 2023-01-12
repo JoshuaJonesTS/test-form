9a263302276e17cc0c0894b1646bed421c848b62
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-12 09:47:31 -0800

    Clean

--- my-app/src/components/Form/form.component.jsx
+++ my-app/src/components/Form/form.component.jsx
@@ -1,8 +1,8 @@
 import { useFormik } from 'formik';
-import * as yup from 'yup';
+// import * as yup from 'yup';
 import TextField from '@mui/material/TextField';
 import { useMutation } from '@apollo/client';
-import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material';
+import { Typography, /*AppBar,*/ Button, /*Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container*/ } from '@mui/material';
 
 const CreateCustomerForm = () => {
     const [createCustomer, {data, loading, error}] = useMutation();
