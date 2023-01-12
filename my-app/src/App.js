a3b83560596792201f88fbe0b1c457c6dd7dca06
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-12 09:57:51 -0800

    Installations

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
+import CreateCustomerForm from './components/Form/form.component';
 
 function App() {
   return (
     <div className="App">
-      
+      <CreateCustomerForm/>
     </div>
   );
 }
