033defeb1583499976111d32036c28d8ac209e9f
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-12 10:35:58 -0800

    Import utility file

--- my-app/src/components/List/list.component.jsx
+++ my-app/src/components/List/list.component.jsx
@@ -1,6 +1,6 @@
 import { useQuery } from '@apllo/client';
 // import { ALL_CUSTOMERS } from 'graphql/customer/queries';
-// import { createRows } from './AllCustomersList.collection.utils.jsx';
+import createRows from './list..collection.utils';
 
 const AllCustomerList = () => {
     const { data, loading, error } = useQuery();
@@ -9,7 +9,7 @@ const AllCustomerList = () => {
 
     let rows = []
     if(data && data.allCustomers) {
-        /*rows = [...createRows(data.allCustomers)];*/
+        rows = [...createRows(data.allCustomers)];
     }
 
     return (
