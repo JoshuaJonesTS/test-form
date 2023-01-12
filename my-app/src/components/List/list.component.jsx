333196f4a7a1e0d4f799fb71caebb2938ccf65b8
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-12 10:15:14 -0800

    Create function

--- my-app/src/components/List/list.component.jsx
+++ my-app/src/components/List/list.component.jsx
@@ -1,3 +1,7 @@
 import { useQuery } from '@apllo/client';
 // import { ALL_CUSTOMERS } from 'graphql/customer/queries';
 // import { createRows } from './AllCustomersList.collection.utils.jsx';
+
+const AllCustomerList = () => {
+    
+}
