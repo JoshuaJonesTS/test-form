097ebca32ccdde9627b78bdd12c55e471b598989
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-12 10:18:09 -0800

    Create variables

--- my-app/src/components/List/list.component.jsx
+++ my-app/src/components/List/list.component.jsx
@@ -3,5 +3,12 @@ import { useQuery } from '@apllo/client';
 // import { createRows } from './AllCustomersList.collection.utils.jsx';
 
 const AllCustomerList = () => {
-    
+    const { data, loading, error } = useQuery();
+
+    if(loading) {return /*<Loader />*/}
+
+    let rows = []
+    if(data && data.allCustomers) {
+        /*rows = [...createRows(data.allCustomers)];*/
+    }
 }
