b06c9ea187757488cf2fdbacb61d76a6582f5d40
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-12 09:28:23 -0800

    Create function

--- my-app/src/components/Form/form.component.jsx
+++ my-app/src/components/Form/form.component.jsx
@@ -2,3 +2,7 @@ import { useFormik } from 'formik';
 import * as yup from 'yup';
 import TextField from '@mui/material/TextField';
 import { useMutation } from '@apollo/client';
+
+const CreateCustomerForm = () => {
+   
+}
